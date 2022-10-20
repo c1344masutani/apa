81f98395db3dfd9e8537c538957d76d7069ec9e6
Masutani Akihiro <c1344@oic.jp>
2022-10-20 11:50:20 +0900

    jjj

--- apa/abc.Designer.cs
+++ apa/abc.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace apa
 {
-    partial class Form1
+    partial class abc
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -34,7 +34,7 @@ namespace apa
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(525, 105);
+            this.button1.Location = new System.Drawing.Point(280, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
--- apa/abc.cs
+++ apa/abc.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace apa
 {
-    public partial class Form1 : Form
+    public partial class abc : Form
     {
-        public Form1()
+        public abc()
         {
             InitializeComponent();
         }
